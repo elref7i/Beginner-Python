d55b20485e005c60ac4f058fd6393deb875cd777
Ahmed re <momo3956248@gmail.com>
2024-04-17 18:27:07 +0200

    #################### EX 1  ############### "

---
+++ Local Scope & Global Scope.py
@@ -0,0 +1,24 @@
+            #################### EX 1  ###############
+"""
+V1 = 1 #Local Scope
+
+
+def fun1():
+    V1 = 1     #Global Scope
+    print(f"inside fun1 = {V1}")
+
+
+fun1()
+print(f"Outside fun1 = {V1}")
+
+"""
+            #################### EX 2 ###############
+
+# def fun1():
+#     V1 = 1  #Global Scope
+#     print(f"inside fun1 = {V1}")
+#
+#
+# fun1()
+# print(f"Outside fun1 = {V1}")  #error
+#
